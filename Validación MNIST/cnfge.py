e12f73b67b2b3b9300ba58f9322a5b3343306139
Hugo RS <64310941+hugorsnchz@users.noreply.github.com>
2021-06-25 18:18:13 +0200

    Add files via upload

---
+++ Validación MNIST/cnfge.py
@@ -0,0 +1,31 @@
+import torch
+
+# Solución de errores #############################################################################
+
+# "RuntimeError: error in LoadLibraryA" t.cat
+import ctypes
+ctypes.cdll.LoadLibrary('caffe2_nvrtc.dll') 
+
+import matplotlib
+matplotlib.use('TkAgg', force=True)
+
+# Aceleración GPU #################################################################################
+
+global device
+
+# [Falta imprimir características de la GPU usada.]
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+    print("Running on the GPU.")
+else:
+    device = torch.device("cpu")
+    print("Running on the CPU.")
+
+# Parámetros ######################################################################################
+
+global image_dimension, image_layers, output_size, age_span
+
+image_dimension = 28    # Dimensión cuadrada plana de la imagen inicial.
+image_layers = 1        # Número de capas (1 = B&W, 3 = RGB).
+output_size = 10        # Dimensión  del vector de salida de la red.
+epochs = 10
---
+++ Validación MNIST/net_functions.py
@@ -0,0 +1,20 @@
+import torch.nn as nn
+import torch.optim as optim
+
+import cnfge
+from class_net import Net
+
+def create_net(ADN): # Crea la red con el ADN y fija los param. de entrenamiento.
+
+    net = Net(ADN).to(cnfge.device)
+    net.optimizer = optim.Adam(net.parameters(), lr = ADN["lr"])
+    net.loss_function = nn.CrossEntropyLoss()
+
+    print('--------------------------------------------------------------')
+    print('Convolutional neural network created.')
+    print('- Model name:', net.model_name)
+    print('- Procesor:', cnfge.device)
+    print('- ADN:', net.ADN)
+    print('-', net)
+
+    return net
